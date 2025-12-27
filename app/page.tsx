5351b2796992a771d8a62e3b80d6bc2922bba580
imad-ice <imads8444@gmail.com>
2025-12-27 08:06:59 +0530

    Fix build errors: add Suspense to login and force-dynamic to pages

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import FeaturedDesigners from "@/components/home/FeaturedDesigners";
 import Testimonials from "@/components/home/Testimonials";
 import HowItWorks from "@/components/home/HowItWorks";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <div className="bg-gray-50 selection:bg-[#FFC629] selection:text-black">
